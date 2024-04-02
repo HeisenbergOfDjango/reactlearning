2054460be8f4fdf2ccdfbddfb46564bc64206616
Aditya Yadav <89907301+adityayadav1234@users.noreply.github.com>
2024-04-03 02:54:34 +0530

    day3 event handler

--- reactlearning/src/App.js
+++ reactlearning/src/App.js
@@ -3,10 +3,14 @@ import './App.css';
 import User from './User';
 
 function App() {
+  const demo = () => {
+    alert("message")
+  }
   return (
     <div className="App">
-      <h1> hello world </h1>
-      <User/>
+      <h1> React Event handler </h1>
+      <button onClick={demo}>button</button>
+      
     </div>
   );
 }
