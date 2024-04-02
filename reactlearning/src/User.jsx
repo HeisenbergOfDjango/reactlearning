cb1e78ea7f1b87aea203b703208a73b311b4deb0
Aditya Yadav <89907301+adityayadav1234@users.noreply.github.com>
2024-04-03 02:45:00 +0530

    day3 jsx file

---
+++ reactlearning/src/User.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const User = () => {
+  
+  return(
+    React.createElement('h1',null,"hello", React.createElement('p', null, "decription"))
+  )
+
+}
+
+export default User
