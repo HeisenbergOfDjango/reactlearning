a505edac1097c6e4959837095afb2e485248d900
Aditya Yadav <89907301+adityayadav1234@users.noreply.github.com>
2024-04-04 03:13:57 +0530

    day4 props in react example 3

--- reactlearning/src/App.js
+++ reactlearning/src/App.js
@@ -1,28 +1,16 @@
+import { useState } from 'react';
 import './App.css';
 import UserCard from './UserCard';
 
 function App() {
 
-  const userData = [
-    {name: "aditya", age: 23, email: "a@b.com"},
-    {name: "amit", age: 25, email: "a@b2.com"},
-    {name: "aayush", age: 24, email: "a@b3.com"},
-  ]
+  const [name, setName] = useState("intial name")
 
   return (
     <div className="App">
-      <h1> React Props Example 2 </h1>
-      {
-        userData.map((user,index) => (
-          <UserCard
-            key = {index}
-            name = {user.name}
-            age = {user.age}
-            email = {user.email}
-          />
-        )
-      )
-      }
+      <h1> React Props Example 3 </h1>
+      <UserCard name={name}/>
+      <button onClick={()=>setName("Final name")} >Update props</button>
     </div>
   );
 }
--- reactlearning/src/UserCard.jsx
+++ reactlearning/src/UserCard.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 
 const UserCard = (props) => { //can give any name to props
-  const {name, age, email} = props
+  
   return (
     <div>
-      <h1>hello Name, Age, Email</h1>
-      <h1>Hello {name}, {age}, {email}</h1>
+        <h1>Hello, {props.name}</h1>
     </div>
   )
 }
