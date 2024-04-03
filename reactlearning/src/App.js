5d98bc37ca046998867ccab331c37c118219cc69
Aditya Yadav <89907301+adityayadav1234@users.noreply.github.com>
2024-04-04 02:54:09 +0530

    day4 props in react example 2

--- reactlearning/src/App.js
+++ reactlearning/src/App.js
@@ -3,12 +3,26 @@ import UserCard from './UserCard';
 
 function App() {
 
-  console.log("---- render ----")
+  const userData = [
+    {name: "aditya", age: 23, email: "a@b.com"},
+    {name: "amit", age: 25, email: "a@b2.com"},
+    {name: "aayush", age: 24, email: "a@b3.com"},
+  ]
 
   return (
     <div className="App">
-      <h1> React Props </h1>
-      <UserCard name="aditya"/>
+      <h1> React Props Example 2 </h1>
+      {
+        userData.map((user,index) => (
+          <UserCard
+            key = {index}
+            name = {user.name}
+            age = {user.age}
+            email = {user.email}
+          />
+        )
+      )
+      }
     </div>
   );
 }
